b5d3f97b5efdcc98b1ba57a9102fb790a9509fd1
Alana <rcclewu@gmail.com>
2025-01-23 21:42:57 -0800

    done

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -5,3 +5,10 @@
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
 const counter = require('../src/counter')
+test('counter(10,increment) will eventualy return 10',()=>{
+    return expect(counter(4,'increment')).resolves.toBe(4)
+})
+
+test('counter(10, decrement) will eventually return 0',()=>{
+    return expect(counter(4,'decrement')).resolves.toBe(0)
+})
--- tests/rainCheck.test.js
+++ tests/rainCheck.test.js
@@ -5,26 +5,26 @@ const rainCheck = require('../src/rainCheck')
 
 describe('Weather check', () => {
   test(`should return "Bring an umbrella" when it is raining`, () => {
-    // expect here
+    expect(rainCheck(1)).toBe("Bring an umbrella.")
   });
 
   test(`should return "No need for an umbrella" when it is not raining`, () => {
-    // expect here
+    expect(rainCheck(false)).toBe("No need for an umbrella.")
   });
 
   test(`should return "Bring a sturdy umbrella, it's windy." when it's raining and windy`, () => {
-    // expect here
+    expect(rainCheck(1,25)).toBe("Bring a sturdy umbrella, it's windy.")
   });
 
   test(`should return "Bring an umbrella and a warm coat, it's cold." when it's raining and cold`, () => {
-    // expect here
+    expect(rainCheck(1,12,6)).toBe(`Bring an umbrella and a warm coat, it's cold.`)
   });
 
   test(`should return "No umbrella needed, but stay hydrated—it's hot outside!" when it's hot and not raining`, () => {
-    // expect here
+    expect(rainCheck(0,25,60)).toBe(`No umbrella needed, but stay hydrated—it's hot outside!`)
   });
 
   test(`should return "No need for an umbrella, but it's quite windy. Hold onto your hat!" when it's windy and not raining`, () => {
-    // expect here
+    expect(rainCheck(0,35,60)).toBe(`No need for an umbrella, but it's quite windy. Hold onto your hat!`)
   });
 });
